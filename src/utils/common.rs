f6ce25009197f7fc99f68af055d791913d2ae0ca
LucasSanchez82 <08.lucas.sanchez@gmail.com>
2025-05-03 13:53:31 +0200

    refacto: remove unused code

--- src/utils/common.rs
+++ src/utils/common.rs
@@ -1,15 +1,6 @@
-use sha2::digest::consts::U4;
-
 use crate::utils::coloryze::Coloryze;
 
 pub fn ask(question: &str) -> String {
-    println!("{}", question);
-    let mut input = String::new();
-    std::io::stdin().read_line(&mut input).unwrap();
-    return input;
-}
-
-pub fn ask_password(question: &str) -> String {
     println!("{}", question);
     let mut input = String::new();
     std::io::stdin().read_line(&mut input).unwrap();
